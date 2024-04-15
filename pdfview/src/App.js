e07367fa2d1048a3544c458d34b3ac0edc2407ce
swapnil chavan <chavanswapnil822@gmail.com>
2024-04-15 17:35:46 +0530

    qr added

--- pdfview/src/App.js
+++ pdfview/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+import QRCode from 'qrcode.react';
+
 
 function App() {
+
+  const link = 'http://62.72.59.146:3006/Seahorse.pdf';
   return (
     <div className="App">
       <h1>PDF Viewer</h1>
     <p>Click the link below to view the PDF:</p>
-    <a href="Swapnil-Chavan-Resume.pdf" target="_blank">View PDF</a>
+    {/* <a href="Swapnil-Chavan-Resume.pdf" target="_blank">View PDF</a> */}
     <a href="Seahorse.pdf" target="_blank">View PDF</a>
+
+    <h1>QR Code</h1>
+      <QRCode value={link} />
     </div>
   );
 }
