bd8301258cee8b930a7f868de9cf18f77b88cd04
swapnil chavan <chavanswapnil822@gmail.com>
2024-04-15 17:00:44 +0530

    updates

---
+++ pdfview/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>PDF Viewer</h1>
+    <p>Click the link below to view the PDF:</p>
+    <a href="Swapnil-Chavan-Resume.pdf" target="_blank">View PDF</a>
+    <a href="Seahorse.pdf" target="_blank">View PDF</a>
+    </div>
+  );
+}
+
+export default App;
